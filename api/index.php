a01e26c023b7b5dff6af310a96324eae5735ea49
Pewwww53 <pewwww53@email.com>
2026-08-02 09:19:07 +0800

    Configure Laravel for Vercel filesystem

--- api/index.php
+++ api/index.php
@@ -1,3 +1,15 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+$tmpDirectories = [
+    '/tmp/views',
+    '/tmp/cache',
+    '/tmp/sessions',
+];
+
+foreach ($tmpDirectories as $directory) {
+    if (! is_dir($directory)) {
+        mkdir($directory, 0755, true);
+    }
+}
+
+require __DIR__.'/../public/index.php';
